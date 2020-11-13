5d3023d08c20bca3d1e0d69d312d7be402b3c1d8
surakshita <72905080+surakshita@users.noreply.github.com>
2020-11-13 17:00:22 +0530

    test-nov-13

--- Test/Eight.java
+++ Test/Eight.java
@@ -4,7 +4,7 @@ public static void detail(String name)
 {
 System.out.println("arg 0:"+name);
 }
-public static void details(String name,int age)
+public static void detail(String name,int age)
 {
 System.out.println("value 0:"+name);
 System.out.println("value 1:"+age);
