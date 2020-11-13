091e0645ae4f3edae4a2cf3558a2884e2b33d120
surakshita <72905080+surakshita@users.noreply.github.com>
2020-11-13 17:09:10 +0530

    test-nov-13

--- Test/Ans5.java
+++ Test/Ans5.java
@@ -2,8 +2,10 @@ public class Ans5
 {
 //encapsulation where statements and methods are wrapped into one unit declaring or making field  as private.
 private static double money=600.0;
-}
+
 public static void getMoney()
 {
+
 return money;
+}
 }
